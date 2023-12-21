40a2363ebac9ea88c3a6d8a8f80e7c743fc51122
Philipp Wagner <pwag86@googlemail.com>
2023-12-21 09:54:37 +0100

    Move all Scripts to Powershell

---
+++ makeSdk.ps1
@@ -0,0 +1,17 @@
+<# Licensed under the MIT license. See LICENSE file in the project root for full license information.#>
+
+# Kiota Executable
+$kiota_exe=kiota
+
+# Parameters for the Code Generator
+$param_openapi_schema="https://localhost:5000/odata/openapi.json"
+$param_language=csharp
+$param_namespace=RebacExperiments.Shared.ApiSdk 
+$param_out_dir="${PSScriptRoot}/src/Shared/RebacExperiments.Shared.ApiSdk"
+$param_log_level=Trace
+
+# Construct the "kiota generate" Command
+$cmd = "${kiota_exe} generate --openapi ${param_openapi_schema} --language ${param_language} --namespace-name ${param_language} --log-level ${param_log_level} --output ${param_out_dir}"
+
+# Run the the "kiota generate" Command
+Invoke-Expression $cmd
